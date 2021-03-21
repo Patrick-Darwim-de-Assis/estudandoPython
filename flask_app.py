30289195012ee0425798d122c95c611c56692937
Patrick Darwim de Assis <patrickdarwimdeassis@gmail.com>
2021-03-21 02:46:21 +0000

    A first cut at the template

--- flask_app.py
+++ flask_app.py
@@ -1,11 +1,11 @@
 
 # A very simple Flask Hello World app for you to get started with...
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Que a esperança de Deus nunca deixe de estar presente em vós'
+def index():
+    return 'Que a esperança de Deus nunca deixe de estar presente em vós </br><3'+render_template("main_page.html")
 
