4db197adeabc3927387ac5141164b50992202ccc
Patrick Darwim de Assis <patrickdarwimdeassis@gmail.com>
2021-03-21 20:52:45 +0000

    Replaced dummy comments with some in-memory storage

--- flask_app.py
+++ flask_app.py
@@ -1,12 +1,20 @@
 
 # A very simple Flask Hello World app for you to get started with...
 
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, request
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
-@app.route('/')
+comments = []
+
+@app.route('/', methods = ["POST", "GET"])
 def index():
-    return 'Que a esperança de Deus nunca deixe de estar presente em vós </br><3'+render_template("main_page.html")
+    if request.method == "GET":
+        return 'Que a esperança de Deus nunca deixe de estar presente em vós </br><3'+render_template("main_page.html", comments=comments)
+
+    comments.append(request.form["contents"])
+    return redirect(url_for('index'))
+
+
 
